379941ad254509068c680569bcc2f9307f708c8e
admin <clementbrohon@gmail.com>
2017-01-21 23:14:35 +0100

    version de sauvegarde sans multi

--- index.js
+++ index.js
@@ -1,130 +1,12 @@
 //gestion de la DB
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/KastleofSand');
-var ObjectId = require('mongoose').Types.ObjectId; 
+var ObjectId = require('mongoose').Types.ObjectId; */
 
 /*****************************************************************************************************
     Objets
 ******************************************************************************************************/
 
-
-function listeNumero_objet() {      
-
-  	this.return_all = function() { 
-  		var deferred = Q.defer();
-  		listeNumero.find()
-  		.exec(function(err,fiches){
-  			if (err==true){
-  				deferred.reject(err);
-  			}
-  			else{
-  				deferred.resolve(fiches);
-  			}
-  		}); 
-  		return deferred.promise;
-  	};
-  	this.return_one = function(id) { 
-  		var deferred = Q.defer();
-  		listeNumero.findById(id)
-		.exec(function(err,fiches){
-			if (err==true){
-				deferred.reject(err);
-			}
-			else{
-				deferred.resolve(fiches);
-			}
-		});
-  		return deferred.promise;
-  	};
-  	this.numero_is_unique = function(nouveauNumero) { 
-  		var deferred = Q.defer();
-  		listeNumero.find({numero:nouveauNumero.numero}).count()
-		.exec(function(err,fiches){
-			if (err==true){
-				deferred.reject("erreur lors de la vérification de l unicité du titre, problème base de données. Vueillez ré-essayer s il vous plait");
-			}
-			else{
-				if (fiches == 0){
-					deferred.resolve(fiches);
-				}
-				else {
-					deferred.reject("Titre déjà existant, choisissez un titre unique.");
-				}
-			}
-		});
-  		return deferred.promise;
-  	};
-  	this.new_numero = function(nouveauNumero) { 
-  		var deferred = Q.defer();
-  		nouveauNumero.save(function(err){
-  			if (err){
-  				deferred.reject('erreur lors de l enregistrement du produit dans la base de donnée. Vueillez ré-essayer s il vous plait');
-  			}
-  			else{
-  				deferred.resolve('');
-  			}
-  		});
-  		return deferred.promise;
-  	};
-  	this.supr = function (titre_entre) {
-  		var deferred = Q.defer();
-  		Produit.remove({ titre: titre_entre })
-		.exec(function(err,fiches){
-			if (err){
-				deferred.reject('err_querry_base_donnee');
-			}
-			else{
-				deferred.resolve('');
-			}
-	  	})
-	  	return deferred.promise;
-	};
-	this.return_one_libre = function() { 
-  		var deferred = Q.defer();
-  		listeNumero.findOne({'avancement': "libre"})
-		.exec(function(err,fiches){
-			if (err==true){
-				deferred.reject(err);
-			}
-			else{
-				deferred.resolve(fiches);
-			}
-		});
-  		return deferred.promise;
-  	};
-  	this.rendez_vous = function(numero_id) { 
-  		console.log(numero_id);
-  		var deferred = Q.defer();
-  		listeNumero.update({'_id': numero_id},  {
-			$set: { "avancement": "rendez-vous" }
-		}) 
-		.exec(function(err){ 
-			if (err){ 
-				deferred.reject(err);
-			} 
-			else{ 
-				deferred.resolve('');
-			} 
-		}) 
-  		return deferred.promise;
-  	};
-  	this.avancement_liberer = function(numero_id) { 
-  		console.log(numero_id);
-  		var deferred = Q.defer();
-  		listeNumero.update({'_id': numero_id},  {
-			$set: { "avancement": "libre" }
-		}) 
-		.exec(function(err){ 
-			if (err){ 
-				deferred.reject(err);
-			} 
-			else{ 
-				deferred.resolve('');
-			} 
-		}) 
-  		return deferred.promise;
-  	};
-}
 /*****************************************************************************************************
     gestion du serveur HTTP
 ******************************************************************************************************/
@@ -165,48 +47,50 @@ app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/views/ex11.html');
 });
 
-/*****************************************************************************************************
-    gestion des POSTS
-******************************************************************************************************/
+var partie = function(){
+  var new_round;
+  idRound= 1;
+  var main = function(channel){
+    new_round= setTimeout(()=>{
+      waves(channel);
+    }, 10000)
+  }
 
-app.post('/api/retourne_projet', function(req,res) {
-	var list_Projet = new Projet_objet;
-	
-	list_Projet.retourne(req.body)
-	.then(function(data){
-		res.json(data);
-	}, function(error) {     
-		res.json('err');
+  var waves = function(channel){
+    idRound++;
+    io.sockets.in(channel).emit("incoming_wave", {roundId: idRound});
+    console.log("incoming_wave"+idRound, channel);
+    main(channel);
+  }
+
+  
+}
+
+
+io.sockets.on('connection', function (socket) {
+
+	socket.on('register', function(message){
+		socket.channel= message.channel;
+		socket.join(message.channel);
+    io.sockets.in(socket.channel).emit("init", {board: [[0,0,0],[0,0,0],[0,0,0]], roundId: idRound});
+    console.log("connecter"+ message.channel);
+    main(socket.channel);
+	});
+
+  socket.on('finished', function(message) {
+    console.log(message);
+    if(message && typeof message.roundId === 'number' && message.roundId === idRound){
+  		clearTimeout(new_round);
+  		waves(socket.channel);
+  	}
+  });
+
+	socket.on('disconnect', function () {
+    console.log("user disconnect");
+    clearTimeout(new_round);
 	});
 });
 
 
-/*****************************************************************************************************
-	Update
-******************************************************************************************************/
 
 
-
-/*****************************************************************************************************
-    Fin en faite
-******************************************************************************************************/
-
-
-
-io.on('connection', function (socket) {
-
-  	socket.on('active_phase_client', function (message, fn) {
-  		var list_Projet = new Projet_objet;
-        list_Projet.active_phase(message._id)
-		.then(function(data){
-			socket.broadcast.emit('active_phase_server', message);
-			fn({etat: true, data});
-		}, function(error) {     
-			fn({etat: false, error});
-		});
-    });	
-
-  	socket.on('disconnect', function () {
-
-  	});
-});
